32d0b9e27b2370752757251bd2afc50d9e912428
Skill-Sale <84181697+Skill-Sale@users.noreply.github.com>
2021-05-14 13:04:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const Discord = require('discord.js');
-
-// Create an instance of a Discord client
 const client = new Discord.Client();
-const token = process.env.token
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
+const token = process.env.token;
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
